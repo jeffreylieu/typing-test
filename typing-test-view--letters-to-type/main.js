495be757b1df9c60859c45412c6e8a9fde7d868d
Jeff Lieu <jefflieu@gmal.com>
2017-06-12 11:29:42 -0700

    changes to voew letters to type

---
+++ typing-test-view--letters-to-type/main.js
@@ -0,0 +1,6 @@
+// * end goal display text
+// * string of text
+var viewText='Wise man say, Forgiveness is divine, but never pay full price for late pizza'
+var getText = document.querySelector('h4')
+//* putting string in element
+getText.textContent = viewText
